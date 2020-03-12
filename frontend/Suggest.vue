17ff7dec0172ee4b284dc365eae4072eb082dd45
ekasunandika <13517130@std.stei.itb.ac.id>
2020-03-12 17:31:35 +0700

    add suggest component

---
+++ frontend/Suggest.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <div>
+      <vue-simple-suggest
+        v-model="chosen"
+        :list="simpleSuggestionList"
+        :filter-by-query="true">
+      </vue-simple-suggest>
+    </div>
+    <br>
+  </div>
+</template>
+
+<script>
+import VueSimpleSuggest from 'vue-simple-suggest'
+
+export default {
+  components: {
+    VueSimpleSuggest
+  },
+  data() {
+    return {
+      chosen: ''
+    }
+  },
+  methods: {
+    simpleSuggestionList() {
+      return [
+        'Human',
+        'Traffic Light',
+        'Car',
+        'Dog',
+        'Sign'
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+  .vue-simple-suggest > ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  .vue-simple-suggest.designed {
+    position: relative;
+  }
+
+  .vue-simple-suggest.designed, .vue-simple-suggest.designed * {
+    box-sizing: border-box;
+  }
+
+  .vue-simple-suggest.designed .input-wrapper input {
+    display: block;
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #cde;
+    border-radius: 3px;
+    color: black;
+    background: white;
+    outline:none;
+    transition: all .1s;
+    transition-delay: .05s;
+    font-family: 'Open Sans Regular';
+  }
+
+  .vue-simple-suggest.designed.focus .input-wrapper input {
+    border: 1px solid #aaa;
+  }
+
+  .vue-simple-suggest.designed .suggestions {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 100%;
+    top: calc(100% + 5px);
+    border-radius: 3px;
+    border: 1px solid #aaa;
+    background-color: #fff;
+    opacity: 1;
+    z-index: 1000;
+    font-family: 'Open Sans Regular';
+  }
+
+  .vue-simple-suggest.designed .suggestions .suggest-item {
+    cursor: pointer;
+    user-select: none;
+  }
+
+  .vue-simple-suggest.designed .suggestions .suggest-item,
+  .vue-simple-suggest.designed .suggestions .misc-item {
+    padding: 5px 10px;
+  }
+
+  .vue-simple-suggest.designed .suggestions .suggest-item.hover {
+    background-color: #1E889B !important;
+    color: #fff !important;
+    opacity: 0.8;
+  }
+
+  .vue-simple-suggest.designed .suggestions .suggest-item.selected {
+    background-color: #1E889B;
+    color: #fff;
+  }
+</style>
---
+++ frontend/json.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <suggestions
+      v-model="query"
+      :options="options"
+      :onInputChange="onCountryInputChange">    
+  </div>
+</template>
+
+
+
+<script>
+import Suggestions from 'vue-suggestions'
+
+Vue.use(Suggestions)
+
+export default {
+  data () {
+    let countries = ['Afghanistan', 'Åland Islands', 'Albania', 'Algeria', 'American Samoa', 'AndorrA', 'Angola', 'Anguilla', 'Antarctica', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize']
+    return {
+      query: '',
+      countries: countries,
+      selectedCountry: null,
+      options: {}
+    }
+  },
+  methods: {
+    onCountryInputChange (query) {
+      if (query.trim().length === 0) {
+        return null
+      }
+      // return the matching countries as an array
+      return this.countries.filter((country) => {
+        return country.toLowerCase().includes(query.toLowerCase())
+      })
+    },
+    onCountrySelected (item) {
+      this.selectedCountry = item
+    },
+    onSearchItemSelected (item) {
+      this.selectedSearchItem = item
+    }
+  }
+}
+</script>
